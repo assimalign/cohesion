5eb2d1331a5e73d4b9b5dded429948a62adcf880
Chase Crawford <ccrawford@assimalign.com>
2025-03-13 05:57:27 -0400

    Fixed core unit tests for unix

--- libraries/Core/Assimalign.Cohesion.Core/src/Internal/PathHelper.cs
+++ libraries/Core/Assimalign.Cohesion.Core/src/Internal/PathHelper.cs
@@ -6,8 +6,21 @@ internal static class PathHelper
 {
     private const char dot = '.';
     private static readonly char[] _separators = ['/', '\\'];
-    private static readonly char[] _invalidFileChars = [.. Path.GetInvalidFileNameChars()];
-    private static readonly char[] _invalidPathChars = [.. Path.GetInvalidPathChars(), '<', '>', '?' ];
+    private static readonly char[] _invalidFileChars = [
+        '\"', '<', '>', '|', '\0',
+            (char)1, (char)2, (char)3, (char)4, (char)5, (char)6, (char)7, (char)8, (char)9, (char)10,
+            (char)11, (char)12, (char)13, (char)14, (char)15, (char)16, (char)17, (char)18, (char)19, (char)20,
+            (char)21, (char)22, (char)23, (char)24, (char)25, (char)26, (char)27, (char)28, (char)29, (char)30,
+            (char)31, ':', '*', '?', '\\', '/'
+        ];
+    private static readonly char[] _invalidPathChars = [  // DO NOT use Path.GetInvalidPathChars() - the results differ per platform.
+        '|', '\0',
+        (char)1, (char)2, (char)3, (char)4, (char)5, (char)6, (char)7, (char)8, (char)9, (char)10,
+        (char)11, (char)12, (char)13, (char)14, (char)15, (char)16, (char)17, (char)18, (char)19, (char)20,
+        (char)21, (char)22, (char)23, (char)24, (char)25, (char)26, (char)27, (char)28, (char)29, (char)30,
+        (char)31, 
+        '<', '>', '?' 
+        ];
 
     internal static bool IsDot(char value)
     {
