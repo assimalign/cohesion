14775f6e9997c5f2a613109abd5bee9f9f8713f3
Chase Crawford <ccrawford@assimalign.com>
2026-09-01 07:31:53 -0400

    chare(cleanup): remove old resilience pipeline implementation and related classes
    - Deleted old asynchronous and synchronous resilience pipeline files.
    - Removed old resilience pipeline builder and validation context classes.
    - Eliminated telemetry-related classes and structures associated with the old implementation.
    - Cleaned up unused code to streamline the resilience library.

--- libraries/Resilience/Assimalign.Cohesion.Resilience/src/_old/Internal/Utilities/Constants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Assimalign.Cohesion.Resilience.Internal;
-
-internal static class Constants
-{
-    public const string OptionsValidation = """
-    This call validates the options using the data annotations attributes.
-    Make sure that the options are included by adding the '[DynamicDependency(DynamicallyAccessedMemberTypes.All, typeof(OptionsType))]' attribute to the calling method.
-    """;
-}
--- libraries/Resilience/Assimalign.Cohesion.Resilience/src/_old/Internal/Utilities/DisposeBehavior.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Assimalign.Cohesion.Resilience.Internal;
-
-internal enum DisposeBehavior
-{
-    Ignore,
-    Allow,
-    Reject
-}
